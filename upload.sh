07e479f7b78a0bfd1414a74813ab784ce0efeaa0
YARA <122050843+Yara0202@users.noreply.github.com>
2024-08-31 19:24:13 +0700

    Trigger nad build

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/ginkgo/qassa*.zip ]; then
-      curl -s https://api.telegram.org/7502875437:AAHWCh1hQ9Wt8X3geAYSKk2T9mtRj8s9ehQ/sendMessage -d chat_id=-1002171624083 -d text="Uploading Build $(cd ~/rom/out/target/product/ginkgo/ && ls qassa*.zip)"
-      rclone copy ~/rom/out/target/product/ginkgo/qassa*.zip hk:qassa -P
-      curl -s https://api.telegram.org/7502875437:AAHWCh1hQ9Wt8X3geAYSKk2T9mtRj8s9ehQ/sendMessage -d chat_id=-1002171624083 -d text="Build $(cd ~/rom/out/target/product/ginkgo/ && ls qassa*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/ginkgo/Nusantara*.zip ]; then
+      curl -s https://api.telegram.org/7502875437:AAHWCh1hQ9Wt8X3geAYSKk2T9mtRj8s9ehQ/sendMessage -d chat_id=-1002171624083 -d text="Uploading Build $(cd ~/rom/out/target/product/ginkgo/ && ls nusantara*.zip)"
+      rclone copy ~/rom/out/target/product/ginkgo/Nusantara*.zip hk1:nusantara -P
+      curl -s https://api.telegram.org/7502875437:AAHWCh1hQ9Wt8X3geAYSKk2T9mtRj8s9ehQ/sendMessage -d chat_id=-1002171624083 -d text="Build $(cd ~/rom/out/target/product/ginkgo/ && ls Nusantara*.zip) Uploaded Successfully!"
 fi
