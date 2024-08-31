e77b75bac3107fa340f25f23e9272d4670311cc7
YARA <122050843+Yara0202@users.noreply.github.com>
2024-08-31 15:47:09 +0700

    Trigger build

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/sagit/qassa*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/sagit/ && ls qassa*.zip)"
-      rclone copy ~/rom/out/target/product/sagit/qassa*.zip sagit:qassa -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/sagit/ && ls qassa*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/ginkgo/qassa*.zip ]; then
+      curl -s https://api.telegram.org/7502875437:AAHWCh1hQ9Wt8X3geAYSKk2T9mtRj8s9ehQ/sendMessage -d chat_id=-1002171624083 -d text="Uploading Build $(cd ~/rom/out/target/product/ginkgo/ && ls qassa*.zip)"
+      rclone copy ~/rom/out/target/product/ginkgo/qassa*.zip hk:qassa -P
+      curl -s https://api.telegram.org/7502875437:AAHWCh1hQ9Wt8X3geAYSKk2T9mtRj8s9ehQ/sendMessage -d chat_id=-1002171624083 -d text="Build $(cd ~/rom/out/target/product/ginkgo/ && ls qassa*.zip) Uploaded Successfully!"
 fi
